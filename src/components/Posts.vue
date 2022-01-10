880f55f81300f0240ef727a46f13ef4b81c61a09
Shun Yamazaki <syunn820@gmail.com>
2022-01-10 15:50:53 +0900

    文字色を変更

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -7,7 +7,7 @@
     <v-card>
       <v-container>
         <h1>Qiita</h1>
-        <v-btn flat href="https://qiita.com/sYamaz">https://qiita.com/sYamaz</v-btn>
+        <v-btn class="text-info" flat href="https://qiita.com/sYamaz">https://qiita.com/sYamaz</v-btn>
       </v-container>
     </v-card>
   </v-container>
